0c179e6286fff1fa871ee4f6b35e5f280e326e3a
florishalleriet <florispshalleriet@gmail.com>
2026-05-09 22:08:45 +0200

    fix: move structure, and add/remove imports

--- src/main/java/com/flopasss/pvppingrangelimit/command/PVPPingRangeLimitCommand.java
+++ src/main/java/com/flopasss/pvppingrangelimit/command/PVPPingRangeLimitCommand.java
@@ -1,21 +1,13 @@
 package com.flopasss.pvppingrangelimit.command;
 
-import static net.minecraft.commands.Commands.argument;
 import static net.minecraft.commands.Commands.literal;
 
 import com.flopasss.pvppingrangelimit.PVPPingRangeLimit;
-import com.flopasss.pvppingrangelimit.data.PVPPingRangeLimitPlayerData;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import java.util.function.Consumer;
-import java.util.function.Function;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
 
 public class PVPPingRangeLimitCommand {
 
